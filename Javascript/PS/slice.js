fce4e590e01cafce37101d0df2c4f95cbda0ab38
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-04-16 12:30:52 +0900

    js-slice()

---
+++ Javascript/PS/slice.js
@@ -0,0 +1,17 @@
+let campus = input.slice(1).map(line => line.split(''));
+
+//input.slice(1) → 첫 줄(N, M)은 제외하고, 나머지 줄(캠퍼스 정보)만 가져옴
+/*
+input = [
+  '3 5',
+  'IOXPX',
+  'XPXPX',
+  'XXXPX'
+];
+
+campus = [
+  ['I', 'O', 'X', 'P', 'X'],
+  ['X', 'P', 'X', 'P', 'X'],
+  ['X', 'X', 'X', 'P', 'X']
+];
+*/
